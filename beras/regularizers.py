32509832023d4cd8d21443e26569604548bd7c71
berleon <github@leon-sixt.de>
2016-03-16 16:43:30 +0000

    add ActivityInBoundsRegularizer

---
+++ beras/regularizers.py
@@ -0,0 +1,36 @@
+# Copyright 2016 Leon Sixt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from keras.regularizers import Regularizer
+import keras.backend as K
+
+
+class ActivityInBoundsRegularizer(Regularizer):
+    def __init__(self, low=-1, high=1):
+        self.low = low
+        self.high = high
+
+    def __call__(self, loss):
+        activation = self.layer.get_output(True)
+        l = K.switch(activation < self.low, (activation - self.low)**2, 0)
+        h = K.switch(activation > self.high, (activation - self.high)**2, 0)
+        return loss + K.sum(h + l)
+
+    def get_config(self):
+        return {
+            "name": self.__class__.__name__,
+            "low": self.low,
+            "high": self.high
+        }
