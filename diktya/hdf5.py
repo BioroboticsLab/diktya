fafd2954550ea58e327a4b200c576e661ad35845
berleon <github@leon-sixt.de>
2016-11-12 10:56:06 +0100

    Add hdf5 wrapper module

---
+++ diktya/hdf5.py
@@ -0,0 +1,128 @@
+# Copyright 2016 Leon Sixt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import h5py
+import numpy as np
+
+
+def create_hdf5_for(fname, data, chunks=256):
+    h5 = h5py.File(fname, 'w')
+    h5.attrs['__append_pos'] = 0
+    _create_datasets_for(h5, data, chunks)
+    return h5
+
+
+def _create_datasets_for(h5, data, chunks=256):
+    for name, array in data.items():
+        shape = array.shape[1:]
+        h5.create_dataset(
+            name,
+            shape=(1,) + shape,
+            chunks=(chunks,) + shape,
+            maxshape=(None,) + shape,
+            dtype=str(array.dtype))
+
+
+def append_to_hdf5(h5, data):
+    def ensure_enough_space_for(size):
+        for name in data.keys():
+            if len(h5[name]) < size:
+                h5[name].resize(size, axis=0)
+
+    size = len(next(iter(data.values())))
+    begin = int(h5.attrs['__append_pos'])
+    end = begin+size
+
+    for name, array in data.items():
+            ensure_enough_space_for(end)
+            if len(array) != size:
+                raise Exception("Arrays must have the same number of samples."
+                                " Got {} and {}".format(size, len(array)))
+            h5[name][begin:end] = array[:size]
+    h5.attrs['__append_pos'] += size
+    return int(h5.attrs['__append_pos'])
+
+
+def shuffle_hdf5(h5, output_fname, batch_size=100, print_progress=False):
+    h5_shuffled = create_hdf5_for(
+        output_fname, next(iterate_hdf5(h5, 1)))
+
+    for key in h5.attrs.keys():
+        h5_shuffled.attrs[key] = h5.attrs[key]
+
+    h5_shuffled.attrs['__append_pos'] = 0
+
+    nb_samples = int(h5.attrs['__append_pos'])
+    batch_size = min(batch_size, nb_samples)
+    if print_progress:
+        import progressbar
+        bar = progressbar.ProgressBar(max_value=int(nb_samples))
+
+    for i, batch in enumerate(iterate_hdf5(h5, batch_size, shuffle=True, nb_iterations=1)):
+        print(i)
+        print(next(iter(batch.values())).shape)
+        append_to_hdf5(h5_shuffled, batch)
+        if print_progress:
+            bar.update((i+1)*batch_size)
+    h5_shuffled.close()
+
+
+def iterate_hdf5(h5, batch_size, shuffle=False,
+                 nb_iterations=None, fields=None):
+    """
+    Iterates over HDF5 file ``h5``.
+
+    Args:
+        batch_size (int):
+        shuffle (int): randomly shuffle data
+        nb_iterations (int): iterate that many times of the data sets. Defaulft is infinit times
+        fields (list):  list of field names to yield
+    """
+    if fields is None:
+        fields = list(h5.keys())
+    nb_samples = len(h5[fields[0]])
+    indicies = np.arange(nb_samples)
+    if shuffle:
+        np.random.shuffle(indicies)
+    iterations_done = 0
+    stop_iteration = 0
+    i = 0
+    while True:
+        size = batch_size
+        batch = {name: [] for name in fields}
+        while size > 0:
+            nb = min(nb_samples, i + size) - i
+            idx = indicies[i:i + nb]
+            idx = np.sort(idx)
+            if shuffle:
+                shuffle_idx = np.arange(nb)
+                np.random.shuffle(shuffle_idx)
+
+            for name in fields:
+                arr = h5[name][idx.tolist()]
+                if shuffle:
+                    arr = arr[shuffle_idx.tolist()]
+                batch[name].append(arr)
+
+            size -= nb
+            if i + nb >= nb_samples:
+                iterations_done += 1
+                if iterations_done >= nb_iterations:
+                    stop_iteration = True
+                    break
+            i = (i + nb) % nb_samples
+        yield {name: np.concatenate(arrs) for name, arrs in batch.items()}
+        if stop_iteration:
+            break
---
+++ tests/test_hdf5.py
@@ -0,0 +1,55 @@
+
+import h5py
+import numpy as np
+
+from diktya.hdf5 import create_hdf5_for, append_to_hdf5, shuffle_hdf5, iterate_hdf5
+
+
+def test_hdf5(tmpdir):
+    bs = 10
+
+    def get_data(i):
+        np.random.seed(i)
+        return {
+            'idx': np.arange(i, i+bs, dtype='int'),
+            'random': np.random.uniform(-1, 1, (bs, 4, 4)),
+            'complex': np.random.normal(-1, 1, (bs, 8)).astype(np.complex128),
+        }
+
+    fname = str(tmpdir.join('test.hdf5'))
+    h5 = create_hdf5_for(fname, get_data(0))
+
+    append_to_hdf5(h5, get_data(0))
+    assert h5.attrs['__append_pos'] == bs
+
+    append_to_hdf5(h5, get_data(1))
+    assert h5.attrs['__append_pos'] == 2*bs
+
+    append_to_hdf5(h5, get_data(2))
+    assert h5.attrs['__append_pos'] == 3*bs
+
+    for i, batch in enumerate(iterate_hdf5(h5, bs, nb_iterations=1)):
+        data = get_data(i)
+        assert set(data.keys()) == set(batch.keys())
+        for name in data.keys():
+            assert (batch[name] == data[name]).all()
+    assert i == 2
+
+    for i, batch in enumerate(iterate_hdf5(h5, bs, nb_iterations=1, shuffle=True)):
+        data = get_data(i)
+        assert set(data.keys()) == set(batch.keys())
+        for name in data.keys():
+            assert (batch[name] != data[name]).any()
+    assert i == 2
+
+    shuffled_fname = str(tmpdir.join('test_shuffled.hdf5'))
+    shuffle_hdf5(h5, shuffled_fname, print_progress=True)
+    h5_shuffled = h5py.File(shuffled_fname, 'r')
+    print(np.array(h5_shuffled['idx']))
+    assert h5_shuffled['idx'].shape == h5['idx'].shape
+    for i, batch in enumerate(iterate_hdf5(h5_shuffled, bs, nb_iterations=1, shuffle=False)):
+        data = get_data(i)
+        assert set(data.keys()) == set(batch.keys())
+        for name in data.keys():
+            assert (batch[name] != data[name]).any()
+    assert i == 2
